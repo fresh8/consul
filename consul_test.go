aed162192ff7d464a99c4d3319fb97cc364dfdeb
Lee Archer <lee@connected-ventures.com>
2017-09-21 12:26:05 +0100

    Test using node address instead of service address

--- consul_test.go
+++ consul_test.go
@@ -1,6 +1,7 @@
 package consul
 
 import (
+	"errors"
 	"testing"
 	"time"
 
@@ -26,5 +27,32 @@ func TestTagServiceHostPort(t *testing.T) {
 		mCatalog.EXPECT().Service(st.Service, st.Tag, nil).Return([]*api.CatalogService{}, nil, nil).Times(1)
 		_, err := TagServiceHostPort(st.Service, st.Tag)
 		assert.Error(t, ErrNoService, err, "ErrNoService error should be returned if consul returns 0 services")
+	})
+
+	t.Run("no service address and cannot find node", func(t *testing.T) {
+		mCatalog.EXPECT().Service("service1", "", nil).Return([]*api.CatalogService{
+			&api.CatalogService{
+				Node: "node1",
+			},
+		}, nil, nil).Times(1)
+		mCatalog.EXPECT().Node("node1", nil).Return(nil, nil, errors.New("node get error")).Times(1)
+		_, err := TagServiceHostPort("service1", "")
+		assert.Error(t, err, "an error should be returned if there is no service address and node cannot be found")
+	})
+	t.Run("use node address instead of service address", func(t *testing.T) {
+		mCatalog.EXPECT().Service("service2", "", nil).Return([]*api.CatalogService{
+			&api.CatalogService{
+				Node:        "node1",
+				ServicePort: 80,
+			},
+		}, nil, nil).Times(1)
+		mCatalog.EXPECT().Node("node1", nil).Return(&api.CatalogNode{
+			Node: &api.Node{
+				Address: "nodeAddress",
+			},
+		}, nil, nil).Times(1)
+		hostPort, err := TagServiceHostPort("service2", "")
+		assert.NoError(t, err)
+		assert.Equal(t, "nodeAddress:80", hostPort, "the node address should be concatenated with service port")
 	})
 }

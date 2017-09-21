ece5c4ecc5c18b8fc335561b1f7f517d8b55ba14
Lee Archer <lee@connected-ventures.com>
2017-09-21 12:07:16 +0100

    Use subtests instead of scopes for existing tests

--- consul_test.go
+++ consul_test.go
@@ -12,18 +12,19 @@ import (
 
 func TestTagServiceHostPort(t *testing.T) {
 	st := serviceTag{Service: "servicename", Tag: "tagname"}
-	{
+	t.Run("no consul", func(t *testing.T) {
 		_, err := TagServiceHostPort(st.Service, st.Tag)
 		assert.Error(t, err, "lookup should fail as there is no catalog")
 		assert.NotNil(t, serviceCache[st], "the cache should now have an entry for this service")
 		assert.WithinDuration(t, time.Now(), *serviceCache[st].LastError, 1*time.Minute)
-	}
+	})
 	ctrl := gomock.NewController(t)
 	mCatalog := mock_api.NewMockCatalog(ctrl)
 	ConsulCatalog = mCatalog
-	{
+
+	t.Run("no service", func(t *testing.T) {
 		mCatalog.EXPECT().Service(st.Service, st.Tag, nil).Return([]*api.CatalogService{}, nil, nil).Times(1)
 		_, err := TagServiceHostPort(st.Service, st.Tag)
 		assert.Error(t, ErrNoService, err, "ErrNoService error should be returned if consul returns 0 services")
-	}
+	})
 }
